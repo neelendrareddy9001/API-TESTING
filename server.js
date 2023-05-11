8f2973fee7a94086f0e882b104db24fac5226ba7
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-05-11 22:32:34 +0200

    Added Post, Get, Put, Del method to Api

--- server.js
+++ server.js
@@ -6,6 +6,18 @@ app.listen(7000, () => {
     console.log("server is running at port 7000")
 })
 
-app.get('/test', (req,res) => {
-    res.send("get method successfull")
+app.get('/mobiles', (req,res) => {
+    res.send("mobiles data")
+})
+
+app.post('/mobiles', (req, res) => {
+    res.send("mobiles posted")
+})
+
+app.put('/mobiles', (req,res) => {
+    res.send("mobile data updated")
+})
+
+app.delete('/mobiles', (res,req) => {
+    res.send("mobile data deleted")
 })
